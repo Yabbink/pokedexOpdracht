cb7aed760d650c35fc9c2f4807cd65e0939bdc1e
Yannick <yannick.huet@student.graafschapcollege.nl>
2024-03-28 09:48:07 +0100

    js changed

--- script.js
+++ script.js
@@ -5,23 +5,27 @@ $(document).ready(function(){
 
   // Functie om gegevens van een pagina met pokemons met de foto´s, id´s en namen op te halen
   function getPokemonPagina(nummer, isShiny){
+    // offset bepalen door de uitkomst van de paginanummer - 1 * het aantal pokemons te doen om de hoeveelheid pokemons op een pagina te bepalen
     let offset = (nummer - 1) * aantalPokemons;
     console.log(offset);
     let pokeURL = "https://pokeapi.co/api/v2/pokemon?offset=" + offset + "&limit=" + aantalPokemons;
 
-    // AJAX-aanroep om gegevens van de PokeAPI te krijgen
+    // AJAX-aanroep om de standaard gegevens van de PokeAPI te krijgen
     $.ajax({
       url: pokeURL,
       type: 'GET',
       success: function(response){
         console.log(response.results);
+
         // eerst de pagina leegmaken voordat de pokemon op de pagina komen
-
         $(".pokemonHead").empty();
+        
         // loopen over de resultaten en ze laten zien op de pagina
-
         $.each(response.results, function(i, item) {
+          // id bepalen door de offset wat op het eerste moment 0 is + 19 te doen en dat plus 1 om alle foto's te laten zien van de pokemons
           let id = offset + i + 1;
+          console.log(id)
+
           // checken of de shiny status actief is en per status een foto toevoegen
           if(isShiny == true)
           {
@@ -52,16 +56,8 @@ $(document).ready(function(){
   function showPokemonDetails(id, isShiny)
   {
     let pokeURL = "https://pokeapi.co/api/v2/pokemon/" + id;
-    let html = '';
-    
-    // AJAX-aanroep om de details van een Pokemon te krijgen
-    $.ajax({
-      url: pokeURL,
-      type: 'GET',
-      success: function(details){
-        pokemonDetails(details, id, html);
-      }
-    });
+
+    pokemonDetails(id, pokeURL);
   }
 
   // Functie om een Pokemon op te zoeken
@@ -69,199 +65,206 @@ $(document).ready(function(){
   {
     let input = $('#pokeField').val();
     let pokeURL = "https://pokeapi.co/api/v2/pokemon/" + input;
-    let html = '';
 
-    // AJAX-aanroep om een specifieke Pokemon op te zoeken
+    pokemonDetails(input, pokeURL);
+  }
+
+  // Functie om details van een Pokemon weer te geven
+  function pokemonDetails(id, pokeURL)
+  {
+    // AJAX-aanroep om de details van een Pokemon te krijgen die je opzoekt of als je er op klikt
     $.ajax({
       url: pokeURL,
       type: 'GET',
       success: function(details){
-        pokemonDetails(details, input, html);
-      }
-    });
-  }
+        // eerst de variabele pokeimage maken 
+        let pokeImage;
 
-  // Functie om details van een Pokemon weer te geven
-  function pokemonDetails(details, id, html)
-  {
-    // eerst de variabele pokeimage maken 
-    let pokeImage;
+        // checken of de shiny status actief is en per status een foto toevoegen
+        if(isShiny == true)
+        {
+          pokeImage = details.sprites.front_shiny;
+        }
+        else
+        {
+          pokeImage = details.sprites.front_default;
+        }
 
-    // checken of de shiny status actief is en per status een foto toevoegen
-    if(isShiny == true)
-    {
-      pokeImage = details.sprites.front_shiny;
-    }
-    else
-    {
-      pokeImage = details.sprites.front_default;
-    }
+         // eerst de variabele maken voordat er dingen aan worden toegevoegd zodat ik het dadelijk kan toevoegen aan de variabel html
+        let types = "";
 
-    // loopen door de Pokemon-types
-    let types = "";
+        // loopen door de Pokemon-types
+        $.each(details.types, function(i, pokeType){
+         console.log("types");
+         console.log(pokeType);
+         console.log(i);
 
-    $.each(details.types, function(i, pokeType){
-      console.log("types");
-      console.log(pokeType);
-      console.log(i);
+         // eerst checken of de pokemon 1 of meer types heeft en daarop reageren
+         if(i > 0)
+         {
+           types += "";
+         }
 
-      // eerst checken of de pokemon 1 of meer types heeft en daarop reageren
-      if(i > 0)
-      {
-        types += "";
-      }
+         // de span een klasse geven op het moment dat de pokemon het desbetreffende type heeft
+         if(pokeType.type.name.includes("fire"))
+         {
+           types +=  '<span class="fire">';
+         }
+         else if(pokeType.type.name.includes("bug"))
+         {
+           types +=  '<span class="bug">';
+         }
+         else if(pokeType.type.name.includes("poison"))
+         {
+           types +=  '<span class="poison">'
+         }
+         else if(pokeType.type.name.includes("water"))
+         {
+           types +=  '<span class="water">'
+         }
+         else if(pokeType.type.name.includes("grass"))
+         {
+           types +=  '<span class="grass">'
+         }
+         else if(pokeType.type.name.includes("ground"))
+         {
+           types +=  '<span class="ground">'
+         }
+         else if(pokeType.type.name.includes("rock"))
+         {
+           types +=  '<span class="rock">'
+         }
+         else if(pokeType.type.name.includes("steel"))
+         {
+           types +=  '<span class="steel">'
+         }
+         else if(pokeType.type.name.includes("ice"))
+         {
+           types +=  '<span class="ice">'
+         }
+         else if(pokeType.type.name.includes("electric"))
+         {
+           types +=  '<span class="electric">'
+         }
+         else if(pokeType.type.name.includes("dragon"))
+         {
+           types +=  '<span class="dragon">'
+         }
+         else if(pokeType.type.name.includes("ghost"))
+         {
+           types +=  '<span class="ghost">'
+         }
+         else if(pokeType.type.name.includes("psychic"))
+         {
+           types +=  '<span class="psychic">'
+         }
+         else if(pokeType.type.name.includes("normal"))
+         {
+           types +=  '<span class="normal">'
+         }
+         else if(pokeType.type.name.includes("fighting"))
+         {
+           types +=  '<span class="fighting">'
+         }  
+         else if(pokeType.type.name.includes("flying"))
+         {
+           types +=  '<span class="flying">'
+         }
+         else if(pokeType.type.name.includes("dark"))
+         {
+           types +=  '<span class="dark">'
+         }
+         else if(pokeType.type.name.includes("fairy"))
+         {
+           types +=  '<span class="fairy">'
+         }
+         types += pokeType.type.name;
+         types += '</span>';
+        });
 
-      // de span een klasse geven op het moment dat de pokemon het desbetreffende type heeft
-      if(pokeType.type.name.includes("fire"))
-      {
-        types +=  '<span class="fire">';
-      }
-      else if(pokeType.type.name.includes("bug"))
-      {
-        types +=  '<span class="bug">';
-      }
-      else if(pokeType.type.name.includes("poison"))
-      {
-        types +=  '<span class="poison">'
-      }
-      else if(pokeType.type.name.includes("water"))
-      {
-        types +=  '<span class="water">'
-      }
-      else if(pokeType.type.name.includes("grass"))
-      {
-        types +=  '<span class="grass">'
-      }
-      else if(pokeType.type.name.includes("ground"))
-      {
-        types +=  '<span class="ground">'
-      }
-      else if(pokeType.type.name.includes("rock"))
-      {
-        types +=  '<span class="rock">'
-      }
-      else if(pokeType.type.name.includes("steel"))
-      {
-        types +=  '<span class="steel">'
-      }
-      else if(pokeType.type.name.includes("ice"))
-      {
-        types +=  '<span class="ice">'
-      }
-      else if(pokeType.type.name.includes("electric"))
-      {
-        types +=  '<span class="electric">'
-      }
-      else if(pokeType.type.name.includes("dragon"))
-      {
-        types +=  '<span class="dragon">'
-      }
-      else if(pokeType.type.name.includes("ghost"))
-      {
-        types +=  '<span class="ghost">'
-      }
-      else if(pokeType.type.name.includes("psychic"))
-      {
-        types +=  '<span class="psychic">'
-      }
-      else if(pokeType.type.name.includes("normal"))
-      {
-        types +=  '<span class="normal">'
-      }
-      else if(pokeType.type.name.includes("fighting"))
-      {
-        types +=  '<span class="fighting">'
-      }
-      else if(pokeType.type.name.includes("flying"))
-      {
-        types +=  '<span class="flying">'
-      }
-      else if(pokeType.type.name.includes("dark"))
-      {
-        types +=  '<span class="dark">'
-      }
-      else if(pokeType.type.name.includes("fairy"))
-      {
-        types +=  '<span class="fairy">'
-      }
-      types += pokeType.type.name;
-      types += '</span>';
-    });
+        console.log(details.abilities);
 
-    console.log(details.abilities);
-    let abilities = "";
+        // eerst de variabele maken voordat er dingen aan worden toegevoegd zodat ik het dadelijk kan toevoegen aan de variabel html
+        let abilities = "";
 
-    // loopen door de vaardigheden van de Pokemon
-    $.each(details.abilities, function(i, ability){
-      console.log("test");
-      console.log(i);
-      if(i > 0)
-      {
-        abilities += "<br/>";
-      }
-      abilities += ability.ability.name;
-    });
+        // loopen door de vaardigheden van de Pokemon
+        $.each(details.abilities, function(i, ability){
+         console.log("test");
+         console.log(i);
 
-    // eerst de variabele stats maken voordat de waarden later in de span worden gezet
-    let stats = "";
-    console.log(details.stats);
+         // eerst checken of de pokemon 1 of meer types heeft en daarop reageren met een br dat het weer op een nieuwe regel wordt gezet
+         if(i > 0)
+         {
+           abilities += "<br/>";
+         }
+         abilities += ability.ability.name;
+        });
 
-    // loopen door de statistieken van de Pokemon
-    $.each(details.stats, function(i, stat){
-      let statNames = "";
+        // eerst de variabele stats maken voordat de waarden later in de span worden gezet
+        let stats = "";
+        console.log(details.stats);
+
+        // loopen door de statistieken van de Pokemon
+        $.each(details.stats, function(i, stat){
+         let statNames = "";
           
-      // de statistieken afkorten op het moment dat de naam herkent wordt
-      if(stat.stat.name.includes("hp"))
-      {
-        statNames += "HP";
-      }
-      else if(stat.stat.name.includes("attack"))
-      {
-        statNames += "Atk"
-      }
-      else if(stat.stat.name.includes("defense"))
-      {
-        statNames += "Def"
-      }
-      else if(stat.stat.name.includes("special-attack"))
-      {
-        statNames += "Sp.Atk"
-      }
-      else if(stat.stat.name.includes("special-defense"))
-      {
-        statNames += "Sp.Def"
-      }
-      else if(stat.stat.name.includes("speed"))
-      {
-        statNames += "Speed"
-      }
+         // de statistieken afkorten op het moment dat de naam herkent wordt
+         if(stat.stat.name.includes("hp"))
+         {
+           statNames += "HP";
+         }
+         else if(stat.stat.name.includes("attack"))
+         {
+           statNames += "Atk"
+         }
+         else if(stat.stat.name.includes("defense"))
+         {
+           statNames += "Def"
+         }
+         else if(stat.stat.name.includes("special-attack"))
+         {
+           statNames += "Sp.Atk"
+         }
+         else if(stat.stat.name.includes("special-defense"))
+         {
+           statNames += "Sp.Def"
+         }
+         else if(stat.stat.name.includes("speed"))
+         {
+           statNames += "Speed"
+         }
 
-      // een span toevoegen om de stats en de namen daarvan te laten zien
-      stats += '<span>';
-      stats += stat.base_stat;
-      stats += '<br/>';
-      stats += statNames;
-      stats += '</span>';
-    });
+         // een span toevoegen om de stats en de namen daarvan te laten zien
+         stats += '<span>';
+         stats += stat.base_stat;
+         stats += '<br/>';
+         stats += statNames;
+         stats += '</span>';
+        });
 
-    // Weergeven van de pop-up
-    $('.popup').css("display", "flex");
+        // Weergeven van de pop-up
+        $('.popup').css("display", "flex");
 
-    // de details van de popup eerst leegmaken voordat er elementen aan worden toegevoegd
-    $(".detailsHead").empty();
+        // de details van de popup eerst leegmaken voordat er elementen aan worden toegevoegd
+        $(".detailsHead").empty();
 
-    // de variabele html vullen met html elementen
-    html += '<div class="details">'
-    html += '<div class="general"><p class="white">' + id + '</p>' 
-    html += '<p class="white">' + details.name + '</p></div>' 
-    html += '<div class="img"><img src="'+ pokeImage +'"></div>' 
-    html += '<div class="types"><p>Type</p><p class="white">' + types + '</p></div>'  
-    html += '<div class="abilities"><p>Abilities</p><p class="white">' + abilities + '</p></div>' 
-    html += '<div class="stats"><p>Base stats</p><p class="white">' + stats + '</p></div>' 
-    html += '</div>'
+        // eerst weer de variabele html maken 
+        let html = '';
+
+        // de variabele html vullen met html elementen
+        html += '<div class="details">'
+        html += '<div class="general"><p class="white">' + id + '</p>' 
+        html += '<p class="white">' + details.name + '</p></div>' 
+        html += '<div class="img"><img src="'+ pokeImage +'"></div>' 
+        html += '<div class="types"><p>Type</p><p class="white">' + types + '</p></div>'  
+        html += '<div class="abilities"><p>Abilities</p><p class="white">' + abilities + '</p></div>' 
+        html += '<div class="stats"><p>Base stats</p><p class="white">' + stats + '</p></div>' 
+        html += '</div>'
     
-    // de html elementen toevoegen aan de detailshead door middel van de append functie
-    $(".detailsHead").append(html);
+        // de html elementen toevoegen aan de detailshead door middel van de append functie
+        $(".detailsHead").append(html);
+      }
+    });
   }
 
   getPokemonPagina(pagina)
@@ -310,8 +313,9 @@ $(document).ready(function(){
 
   // on click functie voor het klikken op een Pokemon om de details weer te geven
   $(document).on('click', '.pokemon', function(){
-     // de index waarde van de pokemon in de variabele id zetten
-     id = $(this).index() + 1
+     // de id van de pokemon bepalen door de index van de pokemon te pakken waar je op klikt en dan + 1 te doen zodat die nooit op 0 komt
+     //en daarnaast ook de pagina waar de pokemon op zit te bepalen door pagina - 1 * het aantal pokemon te doen  
+     id = $(this).index() + 1 + (pagina - 1) * aantalPokemons
      console.log("hallo")
      console.log(id)
      if (isShiny == true) {
